2051ae073c741e19d4b559a65111e1f7601228b0
KatyJMiles <kjmiles99@gmail.com>
2023-09-27 21:10:03 -0600

    Update water data

--- waterUsage_data.py
+++ waterUsage_data.py
@@ -1,1 +1,12 @@
-https://opendata.fcgov.com/resource/ia5t-gxxe.json
+import json
+from urllib.request import urlopen
+import pandas as pd
+import requests
+from datetime import datetime
+
+url = "https://opendata.fcgov.com/resource/ia5t-gxxe.json"
+data = pd.read_json(url)\
+
+data.to_csv("Data/City_of_Fort_Collins_Water_Demand.csv")
+
+  
